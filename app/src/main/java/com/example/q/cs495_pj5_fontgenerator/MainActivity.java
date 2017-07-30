2bebd1906f3bc50a279e52138c0fbb8731cc2199
JOYN28 <kimyoonseo@kaist.ac.kr>
2017-07-30 13:36:50 +0900

    Initial commit

---
+++ app/src/main/java/com/example/q/cs495_pj5_fontgenerator/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.q.cs495_pj5_fontgenerator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
